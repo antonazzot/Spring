c30e5e1c84ba4efbb34df35f3925df467b2141aa
antonazzot <anton.azzot@yandex.ru>
2021-12-06 13:43:30 +0300

    03-spring-xml-bean-autowired-byName-byType-primary

--- src/main/java/springbackwell/model/Game.java
+++ src/main/java/springbackwell/model/Game.java
@@ -15,8 +15,8 @@ public class Game {
     private String name;
 
     private Map <Player, Set<Attainment>> players;
-//    private GameAdmin gameAdmin;
-      private Player myh;
+    private Player gameAdmin;
+//      private Player myh;
 
     public Game(int id, String name, Map<Player, Set<Attainment>> players) {
         this.id = id;
